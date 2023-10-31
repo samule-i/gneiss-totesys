211b91eb598ac828522646b6a0349adae7ab9361
RonanTC <134142736+RonanTC@users.noreply.github.com>
2023-10-31 16:34:08 +0000

    Secretsmanager (#9)
    * foundations for github to secretsmanager pipeline
    
    * update requirements.txt
    
    * add tests, remove test/ from .gitignore file
    
    * update credentials in secrets.md

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,6 +5,7 @@ resource "aws_lambda_function" "totesys_ingestion" {
   s3_key        = aws_s3_object.lambda_code.key
   handler       = "reader.lambda_handler"
   runtime       = "python3.11"
+  layers        = ["arn:aws:lambda:eu-west-2:133256977650:layer:AWS-Parameters-and-Secrets-Lambda-Extension:11"]
 }
 
 resource "aws_lambda_permission" "allow_s3" {
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,3 +2,13 @@ variable "lambda_name" {
     type = string
     default = "totesys_ingestion"
 }
+
+variable "db_credentials_oltp" {
+  type = string
+  default = "db_credentials_oltp"
+}
+
+variable "db_credentials_olap" {
+  type = string
+  default = "db_credentials_olap"
+}
