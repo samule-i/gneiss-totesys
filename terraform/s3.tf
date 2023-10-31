5b387e3e0c70b878307c05356f8b1ac68848d16f
Samuel Massey <s.massey@protonmail.com>
2023-10-31 10:36:43 +0000

    pg8000 layer stored in the code bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,3 +5,9 @@ resource "aws_s3_bucket" "code_bucket" {
 resource "aws_s3_bucket" "data_bucket" {
   bucket_prefix = "totesys-transform-"
 }
+
+resource "aws_s3_object" "pg8000_layer" {
+  key    = "pg8000_layer.zip"
+  source = "${path.module}/../pg8000_layer.zip"
+  bucket = aws_s3_bucket.code_bucket.id
+}
