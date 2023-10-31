1ec725cfecbb2d8ad86cea3215c0888b3c0bcd3f
Nafisa Hassan Satti <nafisahassansatti@Nafisas-MacBook-Air.local>
2023-10-31 14:15:37 +0000

    added the write to JSON function

---
+++ ingestion_function/write_JSON.py
@@ -0,0 +1,32 @@
+
+import logging
+from botocore.exceptions import ClientError
+import boto3
+
+logger = logging.getLogger('MyLogger')
+logger.setLevel(logging.ERROR)
+
+
+def write_to_ingestion(data, bucket, key):
+    """moves a JSON to a S3 Bucket.
+
+    Keyword arguments:
+    data -- the location of the data
+    bucket -- S3 Bucket to be used
+    key -- the name of the object
+    """
+    try:
+        s3 = boto3.client('s3', region_name='eu-west-2')
+        with open(data, 'rb') as f:
+            s3.put_object(Bucket=bucket, Key=key, Body=f)
+    except ClientError as c:
+        if c.response['Error']['Code'] == 'NoSuchBucket':
+            logger.error(f'No such bucket - {bucket}')
+        else:
+            raise
+    except Exception as e:
+        logger.error(e)
+        raise RuntimeError
+
+
+# write_to_ingestion()
