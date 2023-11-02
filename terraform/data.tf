cb4605307c1d48569d75c64de851d0b3063625d5
Samuel Massey <s.massey@protonmail.com>
2023-11-02 16:22:25 +0000

    fix: remove typo

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,7 +4,7 @@ data "aws_region" "current" {}
 
 data "archive_file" "lambda" {
   type        = "zip"
-  excludes = [ "__pycache__" ]|
+  excludes = [ "__pycache__" ]
   source_dir = "${path.module}/../data_ingestion/src/ingestion"
   output_path = "${path.module}/../ingestion_function.zip"
 }
