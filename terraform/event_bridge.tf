5d04731e3a2c164610c49564c68d95e516029c21
TheCodeNinja007 <mwqais01@gmail.com>
2023-11-03 19:25:17 +0000

    amended eventbridge trigger to 15 minutes

--- terraform/event_bridge.tf
+++ terraform/event_bridge.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "ingestion_lambda_invocation_rule" {
   name                = "ingestion-lambda-invocation-event-rule"
   description         = "triggers ingestion lambda at specified rate"
-  schedule_expression = "rate(2 minutes)"
+  schedule_expression = "rate(15 minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "ingestion_lambda_target" {
