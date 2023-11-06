60fe9f192eff5971fc1002a83e718f165fbe79c8
jambent <benn.james.w@gmail.com>
2023-11-06 10:21:29 +0000

    Adds refactored connection code

--- data_ingestion/src/ingestion/pg8000_conn.py
+++ data_ingestion/src/ingestion/pg8000_conn.py
@@ -1,15 +1,16 @@
-from pg8000 import connect
-
-from ingestion.db_credentials import get_credentials
+from pg8000 import connect, DatabaseError
 
 
-def get_conn(database):
+def get_conn(credentials):
     """Connect to source database"""
-    credentials = get_credentials(database)
-    DB_HOST = credentials['hostname']
-    DB_USER = credentials['username']
-    DB_DB = credentials['database']
-    DB_PASSWORD = credentials['password']
+    try:
+        DB_HOST = credentials['hostname']
+        DB_USER = credentials['username']
+        DB_DB = credentials['database']
+        DB_PASSWORD = credentials['password']
 
-    return (connect(user=DB_USER, host=DB_HOST,
-                    database=DB_DB, password=DB_PASSWORD))
+        return (connect(user=DB_USER, host=DB_HOST,
+                        database=DB_DB, password=DB_PASSWORD))
+    except DatabaseError as e:
+        print('Error connecting to DB')
+        raise e
