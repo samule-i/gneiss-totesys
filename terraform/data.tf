e5342197aad34a7a019df503b147f4386b10ec66
samuel <s.massey@protonmail.com>
2023-11-06 23:31:05 +0000

    fix: set proper zip dir & handler path

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,6 +5,6 @@ data "aws_region" "current" {}
 data "archive_file" "lambda" {
   type        = "zip"
   excludes = [ "__pycache__" ]
-  source_dir = "${path.module}/../data_ingestion/src/ingestion"
+  source_dir = "${path.module}/../data_ingestion/src"
   output_path = "${path.module}/../ingestion_function.zip"
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "totesys_ingestion" {
   role          = aws_iam_role.lambda_role.arn
   s3_bucket     = aws_s3_bucket.code_bucket.id
   s3_key        = aws_s3_object.lambda_code.key
-  handler       = "ingestion.lambda_handler"
+  handler       = "ingestion.ingestion.lambda_handler"
   runtime       = "python3.11"
   environment {
     variables = {
