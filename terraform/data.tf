1298eb2d60e0bead0aa5436e842a613b888e65d3
samuel <s.massey@protonmail.com>
2023-11-08 00:35:28 +0000

    feat: infrastructure changes to support moving to root

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,6 +5,6 @@ data "aws_region" "current" {}
 data "archive_file" "lambda" {
   type        = "zip"
   excludes = [ "__pycache__", "totesys_data_ingestion.egg-info" ]
-  source_dir = "${path.module}/../data_ingestion/src"
+  source_dir = "${path.module}/../ingestion"
   output_path = "${path.module}/../ingestion_function.zip"
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "totesys_ingestion" {
   role          = aws_iam_role.lambda_role.arn
   s3_bucket     = aws_s3_bucket.code_bucket.id
   s3_key        = aws_s3_object.lambda_code.key
-  handler       = "ingestion.ingestion.lambda_handler"
+  handler       = "ingestion.lambda_handler"
   runtime       = "python3.11"
   timeout       = 60 # NOTE this must be less than the period defined in the eventbridge rule
   environment {
