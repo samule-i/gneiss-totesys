be07366b72036b35c13fb9c67a758d03a5c8b4e6
samuel <s.massey@protonmail.com>
2023-11-08 16:32:37 +0000

    basic skeleton of main file

--- json_to_parquet/json_to_parquet.py
+++ json_to_parquet/json_to_parquet.py
@@ -1,2 +1,25 @@
+from json_to_parquet.get_s3_file import json_event()
+
+def fake_fn():
+    pass
+
 def lambda_handler(event, context):
-    print('test function')
+    json_body = json_event(event)
+
+    function_dict = {
+        'address': fake_fn,
+        'counterparty': fake_fn,
+        'currency': fake_fn,
+        'department': fake_fn,
+        'design': fake_fn,
+        'payment': fake_fn,
+        'payment_type': fake_fn,
+        'purchase_order': fake_fn,
+        'sales_order': fake_fn,
+        'sample_tim': fake_fn,
+        'staff': fake_fn,
+        'transaction': fake_fn
+    }
+
+    table_name = json_body['table_name']
+    transformed_data = function_dict[table_name]()
