121395cd97f98841dbad5ba9de2d5608fba8eab0
Samuel Massey <s.massey@protonmail.com>
2023-11-12 14:07:02 +0000

    feat: more thorough logging

--- src/json_to_parquet/json_to_parquet.py
+++ src/json_to_parquet/json_to_parquet.py
@@ -4,21 +4,25 @@ from json_to_parquet.get_s3_file import json_event, bucket_list
 from json_to_parquet.dim_currency import currency_transform
 from json_to_parquet.date_dimension import date_dimension
 from json_to_parquet.write_pq_to_s3 import write_pq_to_s3
-from json_to_parquet.custom_log import logger
+from utils.custom_log import logger
 from json_to_parquet.transformations import (
     transform_address, transform_design,
     transform_sales_order)
 from json_to_parquet.dim_counterparty import dim_counterparty
 from json_to_parquet.dim_staff import dim_staff
+import sys
 
 log = logger(__name__)
 
 
-def fake_fn():
+def fake_fn(data):
+    log.info(f'Processing {data["table_name"]} not implemented. Quitting')
+    sys.exit()
     pass
 
 
 def lambda_handler(event, _):
+    log.info(event)
     out_table_lookup = {
         'address': 'dim_location',
         'counterparty': 'dim_counterparty',
@@ -48,13 +52,21 @@ def lambda_handler(event, _):
     out_bucket: str = os.environ['PARQUET_S3_DATA_ID']
     json_body = json_event(event)
 
-    parquet_keys = bucket_list(out_bucket)
-    table_name = json_body['table_name']
     in_key = event['Records'][0]['s3']['object']['key']
+    if in_key == 'timestamps.json':
+        log.info('Key is timestamps, not processing.')
+        return
+    try:
+        table_name = json_body['table_name']
+    except KeyError:
+        log.error(f'Table_name does not exits in {in_key}')
+        return { "status": 400 }
+
     out_key = in_key.replace(table_name, out_table_lookup[table_name])
     log.info(f'Processing {table_name} to {out_bucket}/{out_key}')
     date_dim_key = 'date/dim_date.parquet'
 
+    parquet_keys = bucket_list(out_bucket)
     if date_dim_key not in parquet_keys:
         log.info('Date parquet not found, creating...')
         df = date_dimension()
