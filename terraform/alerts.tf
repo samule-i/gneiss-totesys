c32b510ea3768caf8b343aec10956a18c501f4d6
Ronan Tiller-Cordy <chanotc@proton.me>
2023-11-14 14:22:37 +0000

    feat: alarms for duration and concurrent execution

--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -1,3 +1,24 @@
+##############################################################################
+# Log groups
+##############################################################################
+resource "aws_cloudwatch_log_group" "json_to_parquet_logs" {
+  name = "/aws/lambda/${aws_lambda_function.json_to_parquet.function_name}"
+  depends_on = [aws_lambda_function.json_to_parquet]
+}
+
+resource "aws_cloudwatch_log_group" "parquet_to_OLAP_logs" {
+  name = "/aws/lambda/${aws_lambda_function.parquet_to_OLAP.function_name}"
+  depends_on = [aws_lambda_function.parquet_to_OLAP]
+}
+
+resource "aws_cloudwatch_log_group" "ingestion_logs" {
+  name = "/aws/lambda/${aws_lambda_function.totesys_ingestion.function_name}"
+  depends_on = [aws_lambda_function.totesys_ingestion]
+}
+
+##############################################################################
+# Alert metrics
+##############################################################################
 resource "aws_cloudwatch_log_metric_filter" "Error_Filter" {
   name           = "ErrorFilter"
   pattern        = "ERROR"
@@ -9,12 +30,6 @@ resource "aws_cloudwatch_log_metric_filter" "Error_Filter" {
     value     = "1"
   }
 }
-
-resource "aws_cloudwatch_log_group" "json_to_parquet_logs" {
-  name = "/aws/lambda/${aws_lambda_function.json_to_parquet.function_name}"
-  depends_on = [aws_lambda_function.json_to_parquet]
-}
-
 
 resource "aws_cloudwatch_log_metric_filter" "JSON_to_Parquet_Error_Filter" {
   name           = "ErrorFilter"
@@ -28,12 +43,6 @@ resource "aws_cloudwatch_log_metric_filter" "JSON_to_Parquet_Error_Filter" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "parquet_to_OLAP_logs" {
-  name = "/aws/lambda/${aws_lambda_function.parquet_to_OLAP.function_name}"
-  depends_on = [aws_lambda_function.parquet_to_OLAP]
-}
-
-
 resource "aws_cloudwatch_log_metric_filter" "Parquet_to_OLAP_Error_Filter" {
   name           = "ErrorFilter"
   pattern        = "ERROR"
@@ -46,30 +55,15 @@ resource "aws_cloudwatch_log_metric_filter" "Parquet_to_OLAP_Error_Filter" {
   }
 }
 
+##############################################################################
+# Alarms
+##############################################################################
 
-resource "aws_cloudwatch_log_group" "ingestion_logs" {
-  name = "/aws/lambda/${aws_lambda_function.totesys_ingestion.function_name}"
-  depends_on = [aws_lambda_function.totesys_ingestion]
-}
-
-
-
-
-resource "aws_sns_topic" "user_updates" {
-  name = "ErrorAlertMessages"
-}
-
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  for_each = toset(split(", ", base64decode(var.sns_emails)))
-  topic_arn = aws_sns_topic.user_updates.arn
-  protocol  = "email"
-  endpoint  = each.value
-}
-
+# Errors
 resource "aws_cloudwatch_metric_alarm" "ingestion_alerts" {
-  alarm_name                = "Error-Alerts"
+  alarm_name                = "Ingestion Error Alert"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = 2
+  evaluation_periods        = 1
   metric_name               = "ErrorMetric"
   namespace                 = "CustomLambdaMetrics"
   period                    = 60
@@ -81,12 +75,12 @@ resource "aws_cloudwatch_metric_alarm" "ingestion_alerts" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "json_to_parquet_alerts" {
-  alarm_name                = "Error-Alerts"
+  alarm_name                = "Transformation Error Alert"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 1
   metric_name               = "JSON-to-parquet Lambda"
   namespace                 = "CustomLambdaMetrics"
-  period                    = 600
+  period                    = 60
   statistic                 = "Sum"
   threshold                 = 1
   alarm_description         = "This metric monitors Errors in the execution of the JSON-to-parquet application."
@@ -95,15 +89,94 @@ resource "aws_cloudwatch_metric_alarm" "json_to_parquet_alerts" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "parquet_to_olap_alerts" {
-  alarm_name                = "Error-Alerts"
+  alarm_name                = "Loader Error Alert"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 1
   metric_name               = "Parquet-to-OLAP Lambda"
   namespace                 = "CustomLambdaMetrics"
-  period                    = 600
+  period                    = 60
   statistic                 = "Sum"
   threshold                 = 1
   alarm_description         = "This metric monitors Errors in the execution of the parquet-to-OLAP application."
   insufficient_data_actions = []
   alarm_actions = [aws_sns_topic.user_updates.arn]
+}
+
+# Duration
+resource "aws_cloudwatch_metric_alarm" "alert_long_duration_ingestion" {
+  alarm_name          = "Ingestion Duration Alert"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 45000  # milliseconds
+  alarm_description   = "This alarm monitors excessive runtime duration"
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+  dimensions = {
+    FunctionName = aws_lambda_function.totesys_ingestion.function_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alert_long_duration_transformation" {
+  alarm_name          = "Transformation Duration Alert"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 45000  # milliseconds
+  alarm_description   = "This alarm monitors excessive runtime duration"
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+  dimensions = {
+    FunctionName = aws_lambda_function.json_to_parquet.function_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alert_long_duration_loader" {
+  alarm_name          = "Loader Duration Alert"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 45000  # milliseconds
+  alarm_description   = "This alarm monitors excessive runtime duration"
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+  dimensions = {
+    FunctionName = aws_lambda_function.parquet_to_OLAP.function_name
+  }
+}
+
+# Concurrent execution
+resource "aws_cloudwatch_metric_alarm" "alert_concurrent_execution_ingestion" {
+  alarm_name          = "Ingestion Concurrent Execution Alert"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ConcurrentExecutions"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 1
+  alarm_description   = "This alarm monitors concurrent execution for the ingestion lambda."
+  alarm_actions       = [aws_sns_topic.user_updates.arn]
+  dimensions = {
+    FunctionName = aws_lambda_function.totesys_ingestion.function_name
+  }
+}
+##############################################################################
+# SNS subscription for alerts
+##############################################################################
+resource "aws_sns_topic" "user_updates" {
+  name = "ErrorAlertMessages"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  for_each = toset(split(", ", base64decode(var.sns_emails)))
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol  = "email"
+  endpoint  = each.value
 }
